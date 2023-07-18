18983d80b63343b8c18d889c50b8c7af16747d6a
jayong1011 <20181612@edu.hanbat.ac.kr>
2023-07-18 15:52:32 +0900

    edge

--- study/edge_server.py
+++ study/edge_server.py
@@ -1,29 +1,40 @@
 from flask import Flask, request, Response
 import cv2
+import numpy as np
 from config import * 
 
 app = Flask(__name__)
 
-data = {}
- 
- # 서버 정상적으로 실행되었는지 확인
-@app.route('/')
-def main():
-    
-    print("ddddd")
+roi_coordinates = (100, 100, 800, 800)
 
+data = {}
+def frame():
     cap = cv2.VideoCapture(0)
     
     while True:
         ret, frame = cap.read(0)
-        print(frame)
+        # frame = cv2.resize(frame,(1024,480))
+        
+        # 검은 화면 출력
+        # black_screen = np.zeros(frame.shape, dtype=np.uint8)
+        
+        start_x, start_y, end_x, end_y = roi_coordinates
+        roi = frame[start_y:end_y, start_x:end_x]
+        
+        _, buffer = cv2.imencode('.jpg', roi ,[int(cv2.IMWRITE_JPEG_QUALITY), 60])
+        frame  = buffer.tobytes()
     
-        #해상도 줄여서 데이터 크기 축소(화질떨어짐)
-        frame = cv2.resize(frame,(640, 480))
-        _, buffer = cv2.imencode('.jpg', frame,[int(cv2.IMWRITE_JPEG_QUALITY), 60])
+        yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # 프레임 반환
+        
+         
 
 
-        return buffer
+
+ # 서버 정상적으로 실행되었는지 확인
+@app.route('/')
+def main():
+        return Response(frame(), mimetype ='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
