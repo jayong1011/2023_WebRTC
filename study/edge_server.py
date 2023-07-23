7a049fc941a999d9c0cefc4a4052103dac01d3d9
jayong1011 <20181612@edu.hanbat.ac.kr>
2023-07-23 15:30:36 +0900

    pixel control

--- study/edge_server.py
+++ study/edge_server.py
@@ -6,48 +6,66 @@ from config import *
 
 app = Flask(__name__)
 
-roi_coordinates = [0, 0, 800, 800]
+roi_coordinates = [400, 400, 1200, 800]
 
 data = {}
 
 fixel = []
 
 def fixel_control(part):
-    if keyboard.is_pressed(80):
-        part[0] = part[0]-100
-        part[1] = part[1]-100
-        
-    elif keyboard.is_pressed(70):
-        part[0] = part[0]+100
-        part[1] = part[1]+100
+    
+    if keyboard.is_pressed('down'):
+        print("down")
+        part[1] = part[1]+50
+        part[3] = part[3]+50
+        print(part[1],"-----------------",part[3])
+        return part
+    
+    elif keyboard.is_pressed('up'):
+        print("up")
+        part[1] = part[1]-50
+        part[3] = part[3]-50
+        print(part[1],"-----------------",part[3])
+        return part
+    
+    elif keyboard.is_pressed('left'):
+        print("left")
+        part[0] = part[0]-50
+        part[2] = part[2]-50
+        print(part[0],"-----------------",part[2])
+        return part
+    
+    elif keyboard.is_pressed('right'):
+        print("right")
+        part[0] = part[0]+50
+        part[2] = part[2]+50
+        print(part[0],"-----------------",part[2])
+        return part
     
     else:
-        
         return part
     
 def frame():
     cap = cv2.VideoCapture(0)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    print("Webcam Resolution: {}x{}".format(int(width), int(height)))
+
     while True:
         ret, frame = cap.read(0)
 
-        
         if ret:
-            output_frame = np.zeros_like(frame, dtype=np.uint8)
-        
+                        
             fixel = fixel_control(roi_coordinates)
             
             start_x, start_y, end_x, end_y = fixel[0], fixel[1], fixel[2], fixel[3]
             
             roi = frame[start_y:end_y, start_x:end_x]
             
-            output_frame[start_y:end_y, start_x:end_x] = roi
-            
-            # cv2.rectangle(output_frame, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)  # 녹색 와이어프레임
-            
         
             _, buffer = cv2.imencode('.jpg', roi ,[int(cv2.IMWRITE_JPEG_QUALITY), 60])
             
-        
             frame  = buffer.tobytes()
     
             yield (b'--frame\r\n'
