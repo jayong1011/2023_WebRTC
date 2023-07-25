5f2e48c0e731a5b6b677cc0635ae1cf657cb228a
jayong1011 <20181612@edu.hanbat.ac.kr>
2023-07-25 15:32:16 +0900

    jounal

--- study/edge_server.py
+++ study/edge_server.py
@@ -11,34 +11,31 @@ def shape_control(center_x, center_y):
     
     if keyboard.is_pressed('left'):
             print("left")
-            center_x -= 10
+            center_x -= 30
     elif keyboard.is_pressed('right'):
         print("right")
-        center_x += 10
-    elif keyboard.is_pressed('up'):
-        center_y += 10
-    elif keyboard.is_pressed('down'):
-        center_y -= 10
-        
+        center_x += 30
     return center_x, center_y
 
 #격자 그리기 
 def wireframe(frame,mask):
-    
-    cell_size = 200
-    height, width = frame.shape[:2]
-    
-    
-    grid_frame = frame.copy()
-    grid_frame[mask != 255] = 0
-    
-    for y in range(0, height, cell_size):
-        for x in range(0, width, cell_size):
-            # 원형 모양으로 자른 부분은 검은색으로 채우기
-            if mask[y, x] == 0:
-                cv2.rectangle(grid_frame, (x, y), (x + cell_size, y + cell_size), (0, 255, 0), 2)
-                        
 
+    grid_frame = np.zeros_like(frame)
+    line_color = (0, 255, 0)
+    line_thickness = 2
+
+
+    for y in range(0, frame.shape[0], 100):
+        cv2.line(grid_frame, (0, y), (frame.shape[1], y), line_color, line_thickness)
+
+    for x in range(0, frame.shape[1], 100):
+        cv2.line(grid_frame, (x, 0), (x, frame.shape[0]), line_color, line_thickness)
+
+    # mask_outside_circle = cv2.bitwise_xor(mask)
+    
+    grid_frame = cv2.bitwise_and(grid_frame, grid_frame, mask=mask)
+    
+    
     return grid_frame
 
 #원형 모양 
@@ -57,30 +54,34 @@ def shape_frame(frame, center_x,center_y):
     
     return masked_frame,mask,center_x, center_y
 
-
+#test
 
 # 원하는 부분 원형으로 출력
 def shape():
     cap = cv2.VideoCapture(0)
     center_x, center_y = 1920 ,960
+    desired_fps = 29
+    
+    cap.set(cv2.CAP_PROP_FPS, desired_fps)
+    
     
     while True:
         ret, frame = cap.read()  
         
         if ret:
+        
             # 원형 모양 생성
             masked_frame , mask, center_x, center_y = shape_frame(frame,center_x,center_y)
+            
         
             # 격자 모양 출력
             grid_frame = wireframe(frame,mask)
             
-            #원형을 제외한 부분 격자 모양 출력
-            result_frame = cv2.addWeighted(masked_frame, 1, grid_frame, 1, 0)
 
+            #원형을 제외한 부분 격자 모양 출력
+            result_frame = cv2.addWeighted(masked_frame,1,grid_frame, 0, 0)
 
             _, buffer = cv2.imencode('.jpg', result_frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
-                
-    
             frame  = buffer.tobytes()
             
             # 웹 서버에서 영상 출력
